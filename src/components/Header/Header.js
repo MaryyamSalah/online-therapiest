c8a8cae6895c9f4f4dbd166c9b55ff6793eab1bd
MaryyamSalah <maryyamslah20@gmail.com>
2023-02-01 15:34:30 +0300

    dropdown button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "./logo.png"
+import DropdownComponent from "./dropDown";
   
 export default function Header() {
  
   return (
-    <div>
-      <h1>HEALING</h1>
-      <nav className="bg-green  px-2 sm:px-4 py-2.5 rounded">
-      <div class="container flex flex-wrap items-center justify-between mx-auto">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact Us</Link>
-        <Link to="/blogs">Blogs</Link>
-        <Link to="/login">LOGIN</Link>
+   
+      
+    <nav className="w-full bg-purple-500 shado">
+     
+      <div className="flex py-3 md:py-5 md:block">
+      
+      <ul className="items-center justify-start space-y-8 md:flex md:space-x-6 md:space-y-0">
+      <img className="w-20 h-20" src={logo} alt="logo" />
+      <h1 className="text-2xl font-bold text-white">Healing</h1>
+      
+      <li className="text-white hover:text-indigo-200 pl-96">
+        <Link to="/">Home</Link></li>
+        <li className="text-white hover:text-indigo-200">
+        <DropdownComponent/></li>
+        <li className="text-white hover:text-indigo-200">
+        <Link to="/contact">Contact Us</Link></li>
+        <li className="text-white hover:text-indigo-200">
+        <Link to="/blogs">Blogs</Link></li>
+        <li className="px-4 py-2 text-white hover:text-black bg-purple-900 rounded-md shadow hover:bg-gray-100">
+        <Link to="/login">LOGIN</Link></li>
+        
+        </ul>
+       
         </div>
       </nav>
-    </div>
+    
   );
 }
 
---
+++ src/components/Header/dropDown.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DropdownComponent() {
+    return (
+        <div className="w-72">
+      
+    </div>
+    );
+}
