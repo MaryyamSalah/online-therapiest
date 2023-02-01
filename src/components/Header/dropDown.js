838298c5de82c39787bea030c3ae934bcddff2dd
MaryyamSalah <maryyamsalah20@gmail.com>
2023-02-01 21:36:29 +0300

    completed navbar

--- src/components/Header/dropDown.js
+++ src/components/Header/dropDown.js
@@ -1,9 +1,35 @@
-import React from "react";
 
-export default function DropdownComponent() {
-    return (
-        <div className="w-72">
-      
+import * as React from 'react';
+import './dropDown.css';
+
+const DropdownComponent = ()=> {
+    
+
+
+
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="dropdown">
+      <button onClick={handleOpen}>About</button>
+      {open ? (
+        <ul className="menu">
+          <li className="menu-item">
+            <button>Career</button>
+          </li>
+          <li className="menu-item">
+            <button>Team</button>
+          </li>
+        </ul>
+      ) : null}
+      {open ? <div>Is Open</div> : <div></div>}
     </div>
-    );
-}
+  );
+};
+
+export default DropdownComponent;
